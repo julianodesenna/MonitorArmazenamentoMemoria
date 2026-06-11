9e3b710c2bb39f6c063ac9cfa4e99b94813ae266
julianodesenna <julianoocorretor@gmail.com>
2026-06-10 21:25:11 -0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "br.com.monitorarmazenamentomemoria"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "br.com.monitorarmazenamentomemoria"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.work:work-runtime-ktx:2.11.0")
+    implementation("androidx.core:core-ktx:1.17.0")
+}
